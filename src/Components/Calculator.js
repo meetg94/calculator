7f03ed82b568df206d789fca544079f7921a463b
bakedegg <meetguleria94@gmail.com>
2022-01-30 20:46:33 -0800

    1

---
+++ src/Components/Calculator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Calculator() {
+
+    let num1 = 
+
+    function add() {
+
+    }
+
+
+  return (
+    <div>
+        <div>
+            <p>Num1: {num1} </p>
+            <button>ADD</button>
+            <button>SUBTRACT</button>
+        </div>
+    </div>
+    )
+    
+};
